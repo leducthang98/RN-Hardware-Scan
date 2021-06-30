224d456926f1b57108e7b846358d37be3d074136
LeDucThang <leducthang98@gmail.com>
2021-07-01 00:20:59 +0700

    add id rule

--- src/util/HardwareCalculator.js
+++ src/util/HardwareCalculator.js
@@ -1,9 +1,34 @@
-function checkSum(byteArray) {
+const CMD_ACK = "(CMD:ACK)*38"
+const CMD_NACK = "(CMD:ACK)*38"
+const CMD_SCAN = "(CMD:SCAN)*6E"
+const CMD_GET_ALL_USER = "(CMD:GETMS)*39"
+
+const checkSum = (msg) => {
     let data = 0x00
-    for (let i = 0; i < byteArray.length; i++)
-        data ^= byteArray[i].charCodeAt(0)
-    return data
+    console.log("msg:", msg)
+    for (let i = 0; i < msg.length; i++)
+        data ^= msg[i].charCodeAt(0)
+
+    dataToUpperCase = data.toString(16).toUpperCase()
+    if (dataToUpperCase.length == 1) {
+        dataToUpperCase = "0" + dataToUpperCase
+    }
+    return dataToUpperCase
 }
 
+const getOTPHardware = (x, y, z) => {
+    let hardwareID = (x - 1412) * (x - 1412) - y
+    let OTP = hardwareID + z * 2
+    return OTP
+}
 
+const getMessageActiveWithCheckSum = (userId, OTP) => {
+    let finalId = userId + OTP
+    let checkSumData = checkSum(`(MS:${finalId})`)
+    return `(MS:${finalId})*${checkSumData}`
+}
+
+const getMessageWithCheckSum = (msg) => {
+    return msg + "*" + checkSum(msg)
+}
 
